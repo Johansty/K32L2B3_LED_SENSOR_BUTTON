24e2018dcfadbf10f145b5b96ba4ec24b87b1105
DESKTOP\Johansty <johantorregrozasc@unimagdalena.edu.co>
2021-09-18 11:44:03 -0500

    Proyecto base para la actividad con el uso de los botones

---
+++ K32L2B31A_Project/source/K32L2B31A_Project.c
@@ -0,0 +1,88 @@
+/*! @file : K32L2B31A_Project.c
+ * @author  Ernesto Andres Rincon Cruz
+ * @version 0.0.000
+ * @date    23/08/2021
+ * @brief   Funcion principal main
+ * @details
+ *			v0.0.000	Proyecto base creado usando MCUXpresso
+ *
+ *
+ */
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "K32L2B31A.h"
+#include "fsl_debug_console.h"
+#include <limits.h>
+#include <float.h>
+
+#include <iot_sdk_peripherals_leds.h>
+#include <iot_sdk_peripherals_light.h>
+#include "iot_sdk_peripherals_buttons.h"
+#include "iot_sdk_peripheral_temperature.h"
+
+#include "iot_sdk_ irq_lptimer0.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Private Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * External vars
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Local vars
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Private Source Code
+ ******************************************************************************/
+int main(void) {
+	float light_value;
+	float temperature_value;
+	bool estado1;
+	bool estado2;
+
+    /* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+    /* Init FSL debug console. */
+    BOARD_InitDebugConsole();
+#endif
+
+    /* Activa LPTMR0 para que iniciar contador y posterior IRQ cada 1 segundo*/
+    LPTMR_StartTimer(LPTMR0);
+
+
+    while(1) {
+    	if(lptmr0_ticks!=0){
+    		lptmr0_ticks=0;
+    		 estado1 = leerBoton1();
+    		 estado2 = leerBoton2();
+    		 if (estado1!=false){
+
+    			 printf("boton1\r\n");
+    			  printf("\r\n");
+    		 }
+
+    		 if (estado2!=false){
+
+    			 printf("boton2\r\n");
+    			  printf("\r\n");
+    		 }
+
+    	}
+    }
+    return 0 ;
+}
